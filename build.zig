7566312123c6adad1616dda5e3bc3a9f8c3039e5
Yuré Vanderbruggen <vanderbruggen.yure@gmail.com>
2025-03-16 19:31:50 +0100

    tried to update the build.zig to cross compile for macos and windows. did not end well => deprecated (as windows should be)

--- build.zig
+++ build.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
 
+//const targets = [_]std.zig.CrossTarget{
+//    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+//    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+//    .{ .cpu_arch = .aarch64, .os_tag = .windows },
+//};
+
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const root_file_path = b.path("src/main.zig");
+
     const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
+        .root_source_file = root_file_path,
+        .target = b.standardTargetOptions(.{}),
         .optimize = optimize,
     });
     const exe = b.addExecutable(.{
@@ -26,6 +33,19 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/tests.zig"),
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
+    const test_step = b.step("test", "Run unitests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    //const exe_step = b.step("exe", "Install executable for all targets");
+    //for (targets) |target| {
+    //    const build_exe = b.addExecutable(.{
+    //        .name = "rek",
+    //        .target = target,
+    //        .optimize = optimize,
+    //        .root_source_file = root_file_path,
+    //    });
+    //    const exe_install = b.addInstallArtifact(build_exe, .{});
+    //    exe_step.dependOn(&exe_install.step);
+    //}
+    //b.default_step.dependOn(exe_step);
 }
